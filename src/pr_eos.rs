a3fe746be31cdca98104b171494a74135233d040
teddy0@archDesktop <tong011@e.ntu.edu.sg>
2023-11-26 15:50:50 +0800

    added coefficients for cubic polynomial

--- src/pr_eos.rs
+++ src/pr_eos.rs
@@ -47,12 +47,12 @@ pub fn get_pressure_peng_robinson_gas(
         // a_t = 0.45724 r^2*T_c^2/P_c * alpha(t)
         // alpha(t) = [1 + kappa * (1- sqrt(T_r))]^2
         // T_r  is reduced temperature
-        let reduced_temeprature: Ratio 
+        let reduced_temperature: Ratio 
         = temperature/critical_temperature;
 
         let sqrt_alpha = Ratio::new::<ratio>(1.0)
         + kappa * (Ratio::new::<ratio>(1.0)
-        - reduced_temeprature.sqrt());
+        - reduced_temperature.sqrt());
 
         let alpha = sqrt_alpha * sqrt_alpha;
 
@@ -80,6 +80,52 @@ pub fn get_molar_volume_peng_robinson_gas(
         
         let b = get_b(critical_pressure, critical_temperature);
         let kappa = get_kappa(accentricity_factor);
+
+        // R
+        let molar_gas_constant = 
+        MolarHeatCapacity::new::<joule_per_kelvin_mole>(8.314);
+
+        // B
+        let capital_b: Ratio = b * pressure / molar_gas_constant 
+        / temperature;
+
+        // A
+        let capital_a: Ratio;
+
+        let kappa = get_kappa(accentricity_factor);
+        let reduced_temperature: Ratio 
+        = temperature/critical_temperature;
+        let sqrt_alpha = Ratio::new::<ratio>(1.0)
+        + kappa * (Ratio::new::<ratio>(1.0)
+        - reduced_temperature.sqrt());
+        let alpha: Ratio = sqrt_alpha * sqrt_alpha;
+
+        let a = 0.45724 * molar_gas_constant 
+        * molar_gas_constant 
+        * critical_temperature 
+        * critical_temperature 
+        / critical_pressure 
+        * alpha;
+
+        capital_a = a * pressure / molar_gas_constant 
+        / molar_gas_constant
+        / temperature
+        / temperature;
+
+        // Z^3 - (1-B) Z^2 + (A - 3B^2 - 2B) Z 
+        // - (AB-B^2 -B^3)
+        let one_mins_capital_b: Ratio 
+        = Ratio::new::<ratio>(1.0) - capital_b;
+        let capital_a_minus_3_cap_b_sq_minus_2_cap_b: Ratio 
+        = capital_a - 3.0 * capital_b * capital_b 
+        - 2.0 * capital_b;
+
+        let cap_ab_minus_cap_b_sq_minus_b_cubed: Ratio 
+        = capital_a * capital_b - 
+        capital_b * capital_b 
+        - capital_b * capital_b * capital_b;
+
+
 
 
 
